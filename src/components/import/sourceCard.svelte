b16f03c8f2002812bbaebb3c72d1427bbab88e0f
Alikia2x <ziwen243@outlook.com>
2024-05-04 19:18:37 +0800

    improve: add sourceCard link

--- src/components/import/sourceCard.svelte
+++ src/components/import/sourceCard.svelte
@@ -2,16 +2,21 @@
     export let title: string;
     export let icon: string;
     export let details: string;
+    export let dest: string;
     import Icon from '@iconify/svelte';
 </script>
 
-<div class="flex relative min-h-20 h-fit p-4 w-full m-4 border-2 border-zinc-400 dark:border-neutral-700 rounded-lg">
-    <div class="flex flex-col justify-center text-4xl">
-        <Icon icon={icon} />
+<a href={dest}>
+    <div
+        class="cursor-pointer flex relative min-h-20 h-fit p-4 w-full m-4 border-2 border-zinc-400 dark:border-neutral-700 rounded-lg"
+    >
+        <div class="flex flex-col justify-center text-4xl">
+            <Icon {icon} />
+        </div>
+
+        <div class="ml-4 flex flex-col justify-center">
+            <h3 class="text-lg font-semibold">{title}</h3>
+            <p>{details}</p>
+        </div>
     </div>
-    
-    <div class="ml-4 flex flex-col justify-center">
-        <h3 class="text-lg font-semibold">{title}</h3>
-        <p>{details}</p>
-    </div>
-</div>
+</a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 
 <div
     class="h-fit min-h-screen w-screen max-w-[100vw] overflow-x-hidden overflow-y-auto 
-    bg-slate-50 dark:bg-[#1f1f1f] text-black dark:text-white"
+    bg-zinc-50 dark:bg-[#1f1f1f] text-black dark:text-white"
 >
     <slot />
 </div>
