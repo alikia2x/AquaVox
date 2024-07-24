a23a52f5e70902ae1ddfc85602214469529fdb60
alikia2x <ziwen243@outlook.com>
2024-07-25 02:36:17 +0800

    ref: better type support in server side script fix: a typo in return message at api/database/song/[id]

--- src/routes/api/database/song/[id]/+server.ts
+++ src/routes/api/database/song/[id]/+server.ts
@@ -1,19 +1,20 @@
 import { getCurrentFormattedDateTime } from '$lib/songUpdateTime';
 import { json, error } from '@sveltejs/kit';
 import fs from 'fs';
+import type { RequestHandler } from './$types';
 
-export async function GET({ params }) {
+export const GET: RequestHandler = async ({ params }) => {
     const filePath = `./data/song/${params.id}.json`;
     if (!fs.existsSync(filePath)) {
         return error(404, {
-            message: "No correspoding song."
+            message: "No corresponding song."
         })
     }
     const data = fs.readFileSync(filePath);
 	return json(JSON.parse(data.toString()));
 }
 
-export async function POST({ params, request }) {
+export const POST: RequestHandler = async ({ request, params }) => {
     const timeStamp = new Date().getTime();
     if (!fs.existsSync("./data/pending/")) {
         fs.mkdirSync("./data/pending");
